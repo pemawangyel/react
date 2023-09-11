5944bce2fe252f59e7dd7a0fe933eef25bfad968
Pema Wangyel <pema.wangyel@selise.ch>
2023-09-11 10:45:59 +0600

    basic of slice

---
+++ IntroToJSES6/file3.js
@@ -0,0 +1,18 @@
+// var tweet = prompt("compose your tweet:")
+// var tweetCount=tweet.length
+// alert("You have written" + tweetCount + " characters. you have " + (140-tweetCount)+" characters remaining" )
+
+
+var tweet = ("kdfkjsdfksjfksjfksjfksdjfkdsfjkdsljfkldsjfksdlfjsdkljfksdljfksaljfkljfkldsjfklsdjfklsdjfkldsjfkldsjfklsdfjkdslfjsdjfkldsfjkldsjfkldsjfkldjfkldsjfklsjfklsjfkldfkldsfkldsjfkldjfklsdjfklsjsf")
+var tweetCount=tweet.length
+console.log(`You have written ${tweetCount} characters. you have ${140-tweetCount} characters remaining`)
+
+var age = 23
+var fullName = "test"
+var standard = 1
+console.log("my name is " +fullName+ " I am currently " +age+ " years old. I am studing in class "+standard )
+console.log(`my namd is ${fullName}, I am studing in class ${standard}. My age is ${age}`)
+
+
+let z = "lkjgngkgkdfg"
+console.log(z.length)
---
+++ IntroToJSES6/slice.js
@@ -0,0 +1,11 @@
+var log = console.log
+
+var fname = 'Tshering Doley'
+log(fname)
+log(fname.slice(0,4))
+
+var age = '79879798798'
+log((age.slice(0,2)))
+
+var tweet = 'This is just a example of slice'
+log(tweet.slice(8,12))
